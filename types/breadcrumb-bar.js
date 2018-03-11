0779746e7deb4d877c41fbb4ccb071156fdc663c
Frank Baak <frank@maxserv.com>
2018-03-11 17:57:27 +0100

    Added support for the Pull Request Overview breadcrumb bar

---
+++ types/breadcrumb-bar.js
@@ -0,0 +1,28 @@
+import {copyToClipboard} from '../lib/copyToClipboard';
+import {linkClassName, createCopyLink} from '../lib/createCopyLink';
+
+const dataAttribute = 'data-stash-copy-buttons-haslink';
+
+window.setInterval(_ => {
+    const breadcrumbs = document.querySelectorAll('.file-content .breadcrumbs:not([' + dataAttribute + ']');
+
+    if (breadcrumbs.length > 0) {
+
+        breadcrumbs.forEach(crumb => {
+            const link = createCopyLink();
+            crumb.prepend(link);
+
+            crumb.setAttribute(dataAttribute, true);
+
+            link.addEventListener('click', _ => {
+                const pathParts = [...crumb.querySelectorAll('.file-path span:not(.sep)')].map(pathNode => {
+                    return pathNode.innerText;
+                });
+
+                pathParts.push(crumb.querySelector('.stub').innerText);
+
+                copyToClipboard(link, pathParts.join('/'));
+            });
+        });
+    }
+}, 500);

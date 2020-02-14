0a39ae44efa9c16b87e2298bb9487c2946bbe23b
Dennis Prinse <dennis@dennisprinse.com>
2020-02-14 19:23:23 +0100

    Updated manifest and web-ext package, added auto-update configuration
    Updated manifest.json with Firefox Addon id and update url, updated web-ext package to latest version, added npm run sign script which creates a signed .xpi addon file for Firefox, moved web-ext config to web-ext-config.js added updates.json file and updated

---
+++ web-ext-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    artifactsDir: 'artifacts',
+    ignoreFiles: [
+        '.git/',
+        '.idea/',
+        'artifacts/',
+        'lib/',
+        'node_modules/',
+        'types/',
+        '.gitignore',
+        'index.js',
+        'LICENSE',
+        'package.json',
+        'package-lock.json',
+        'README.md',
+        'web-ext-config.js',
+        'webpack.config.js'
+    ],
+    build: {
+        overwriteDest: true,
+    },
+    sign: {
+        channel: 'unlisted'
+    }
+};

577a69045f66ad915c79aa8f44fbb3102dc382fd
Frank Baak <frank@maxserv.com>
2022-02-03 21:01:51 +0100

    Also show the copy buttons in comments on the pull request overview page

--- types/breadcrumb-bar.js
+++ types/breadcrumb-bar.js
@@ -3,11 +3,14 @@ import {linkClassName, createCopyLink} from '../lib/createCopyLink';
 import waitForElement from '../lib/waitForElement';
 
 // Breadcrumb bar, file view in pull request
+// Also used in comments on the pull request overview page
 
-waitForElement('.change-header', () => {
-    const breadcrumbs = document.querySelectorAll('.file-breadcrumbs');
+waitForElement('.file-breadcrumbs', () => {
+    const breadcrumbs = document.querySelectorAll('.file-breadcrumbs:not([data-processed-by-stashcopybuttons])');
 
     breadcrumbs.forEach(crumb => {
+        crumb.setAttribute('data-processed-by-stashcopybuttons', true);
+
         const link = createCopyLink();
         link.classList.add(linkClassName + '--baseline');
         crumb.prepend(link);

f35f93efa0c219c567b58d97b40f0935a2e7e333
Dennis Prinse <dennis.prinse@maxserv.com>
2022-02-07 22:03:41 +0100

    Add icons to create-new-pullrequest, fix alignment after-branch-dropdown
    * Add copy icons after branch dropdown in initial view
    * Add copy icons after branch names in breadcrumbs above PR details form
    * Fix alignment of copy button after dropdown

--- types/after-branch-dropdown.js
+++ types/after-branch-dropdown.js
@@ -1,5 +1,5 @@
-import {copyToClipboard} from '../lib/copyToClipboard';
-import {linkClassName, createCopyLink} from '../lib/createCopyLink'
+import { copyToClipboard } from '../lib/copyToClipboard';
+import { createCopyLink, linkClassName } from '../lib/createCopyLink';
 
 // Branch dropdown in commit view
 
@@ -8,8 +8,13 @@ const branchActionsButton = document.querySelector('#branch-actions');
 
 if (dropdown !== null && branchActionsButton !== null) {
     const link = createCopyLink();
-    link.classList.add(linkClassName + '--baseline');
-    branchActionsButton.after(link);
+    link.classList.add(linkClassName + '--fullHeight');
+
+    const linkWrapper = document.createElement('div');
+    linkWrapper.classList.add(linkClassName + '-wrapper');
+    linkWrapper.appendChild(link);
+
+    branchActionsButton.after(linkWrapper);
 
     link.addEventListener('click', _ => {
         copyToClipboard(link, `git fetch && git checkout ${dropdown.title} && git pull`);

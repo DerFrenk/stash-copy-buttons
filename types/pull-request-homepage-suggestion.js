758b736ab743dd958666a28aa75d33f1c0e5968d
Frank Baak <frank@maxserv.com>
2022-11-04 15:28:47 +0100

    Fixed duplicate scb-Links on bitbucket homepage after new commit

--- types/pull-request-homepage-suggestion.js
+++ types/pull-request-homepage-suggestion.js
@@ -4,9 +4,13 @@ import waitForElement from "../lib/waitForElement";
 import { createCheckoutCommand } from "../lib/createCheckoutCommand";
 
 waitForElement(".suggestion-text", () => {
-    const prSuggestions = document.querySelectorAll(".suggestion-text a");
+    const prSuggestions = document.querySelectorAll(
+        ".suggestion-text a:not([data-processed-by-stashcopybuttons]):not(.scb-Link)"
+    );
 
     prSuggestions.forEach((branch) => {
+        branch.setAttribute("data-processed-by-stashcopybuttons", true);
+
         const link = createCopyLink();
         link.classList.add(linkClassName + "--baseline");
         branch.after(link);
